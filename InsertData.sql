5db85f4a5dfea5225d0849be8fbfa1ffe4c89131
Dinh Van Luc <dinhvanluc69@gmail.com>
2020-04-02 23:51:41 +0700

    Generate Code Insert

---
+++ InsertData.sql
@@ -0,0 +1,144 @@
+USE QuanLyThuVien
+
+GO
+
+-- Insert TACGIA
+
+
+-- Insert KESACH
+
+-- Insert THELOAI
+
+-- Insert KHO
+
+-- Insert Nhà cung cấp - NCC
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4441','Skybooks','Số 50 đường 5, TTF 361 An Dương, Tây Hồ, Hà Nội','437172838');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4442','Công ty cổ phần MTV Hà Nội','38, ngách 88b, ngõ 49, phố Thúy Lĩnh, Phường Lĩnh Nam,  Hoàng Mai, Hà Nội','2436805252');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4443','NXB Trẻ ','161B Lý Chính Thắng, Phường 7,Quận 3,Hồ Chí Minh.','2839316289');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4444','Nhà Sách Trí Việt ','234 Tây Sơn, Trung Liệt, Đống Đa, Hà Nội','983928269');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4445','Thiên Long ','Số 7/33 Ngõ 199 Hồ Tùng Mậu - Phường Cầu Diễn - Quận Nam Từ Liêm - Hà Nội','969318982');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4446','Tiki Trading','52 Út Tịch, phường 4, quận Tân Bình, thành phố Hồ Chí Minh','19006035');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4447','Nhà sách Fahasa','60 - 62 Lê Lợi, Quận 1, TP. HCM, Việt Nam','1900636467');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4448','Nhà sách Thanh Hà','Thanh Hà Books NV5.36 khu đô thị Viglacera phường Tây Mỗ quận Nam Từ Liêm, Từ Liêm','0388 553 222');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4449',' ALPHA BOOKS','176 Thái Hà, Trung Liệt, Đống Đa, Hà Nội',' 093 232 99 59');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4450',' AZ VIỆT NAM','Số 56 - Đường 5 - Khu tập thể F361 - phố An Dương - P. Yên Phụ - Q.Tây Hồ - HN','2432567333');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4451','FIRST NEWS TRÍ VIỆT','11H Nguyễn Thị Minh Khai, Phường Bến Nghé, Quận 1, Thành phố Hồ Chí Minh','2838227979');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4452',' Văn Hóa Văn Lang','06 Nguyễn Trung Trực, P.5, Q. Bình Thạnh, TP.HCM',' 02838242157 ');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4453','Nhà sách Thái Hà','119 C5 Tô Hiệu, Khu tập thể Nghĩa Tân, Cầu Giấy, Hà Nội','024 6281 3638');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4454','TÂN VIỆT',' 449 Bạch Mai - Hai Bà Trưng - Hà Nội','2439728108');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4455',' CTY CP MTV HÀ NỘI','số 17 Ngô Quyền – 34 Tràng Tiền, phường Tràng Tiền, quận Hoàn Kiếm, TP Hà Nội','43934063');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4456','PANDA BOOKS','B8/20 Đầm Trấu, Hai Bà Trưng, Hà Nội., Bạch Đằng, Hai Bà Trưng, Hà Nội','024 3856 9432');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4457',' SÁCH MINH LONG','Nhà Số 1, Lô A7, Khu Đô Thị Đầm Trấu, Phường Bạch Đằng, Quận Hai Bà Trưng, Thành Phố Hà Nội','024 6294 3819');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4458','Vinabook JSC','44 Xô Viết Nghệ Tĩnh, Phường 19, Quận Bình Thạnh, TP. Hồ Chí Minh','028 35140632');
+INSERT INTO NCC (MaNCC,TenNCC,DiaChiNCC,SDT_NCC) VALUES ('4459','Nhà Sách Sao Mai','71 Đông Hồ, phường 8, quận Tân Bình, Thành phố Hồ Chí Minh','08 62 935 574');
+
+-- Insert Nhà xuất bản  - NXB
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('551','NXB Phụ Nữ','39 Hàng Chuối, Q. Hai Bà Trưng, Hà Nội','2439710717');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('552','NXB Lao Động ','175 Giảng Võ, Đống Đa, Hà Nội175 Giảng Võ, Đống Đa, Hà Nội','2438515380');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('553','NXB Trẻ','62 Nguyễn Thị Minh Khai, Phường Đa Kao, Quận 1, TP.HCM','2838225340');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('554','NXB Kim Đồng ','55 Quang Trung, Hai Bà Trưng, Hà Nội','2439434730');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('555','NXB Hà Nội','Số 4, phố Tống Duy Tân, quận Hoàn Kiếm, thành phố Hà Nội','438252916');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('556','NXB Tổng hợp thành phố Hồ Chí Minh','62 Nguyễn Thị Minh Khai, Phường Đa Kao, Quận 1, TP.HCM','028 38 225 340');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('557','NXB chính trị quốc gia sự thật','Số 6/86 Duy Tân, Cầu Giấy, Hà Nội; ','28049221');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('558','NXB giáo dục','81 Trần Hưng Đạo, Hà Nội','2438220801');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('559','NXB Hội Nhà văn','Số 65 Nguyễn Du, Hà Nội','2438222135');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('560','NXB Tư pháp','Số 35 Trần Quốc Toản, Hoàn Kiếm, Hà Nội','2462632078');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('561','NXB thông tin và truyền thông','Tầng 6, Tòa nhà Cục Tần số Vô tuyến điện, số 115 Trần Duy Hưng, Hà Nội ','024 35772145');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('562','NXB giao thông vận tải','80B Trần Hưng Đạo, Hoàn Kiếm, Hà Nội','024 3942334');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('563','NXB Đại học Quốc Gia Hà Nội','16 Hàng Chuối, Phạm Đình Hổ, Hai Bà Trưng, Hà Nội',' 024 3971 4896');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('564','NXB Đại học Quốc gia Thành phố Hồ Chí Minh','Khu phố 6, P. Linh Trung, Q. Thủ Đức, TP. HCM','028 6681 7058');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('565','NXB Văn Học','18, Nguyễn Trường Tộ, Trúc Bạch, Ba Đình, Hà Nội','');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('566','NXB Đại học Oxford','Đại học Oxford','');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('567','NXB Đại học Cambridge','Đại học Cambridge','');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('568','NXB Hồng Đức','65, Tràng Thi, Hà Nội','');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('569','NXB Tri Thức','Tầng 1, Tòa nhà VUSTA, 53, Nguyễn Du, Hai Bà Trưng, Hà Nội','024 3945 4661');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('570','NXB QUÂN ĐỘI NHÂN DÂN ','23 Lý Nam Đế, Hoàn Kiếm, Hà Nội','2438455766');
+INSERT INTO NXB (MaNXB,TenNXB,DiaChiNXB,SDT_NXB) VALUES ('571','NXB Thế giới','46 Trần Hưng Đạo, Hàng Bài, Hoàn Kiếm, Hà Nội ','024 3825 3841');
+
+-- Insert THETHUVIEN
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150001','Còn Hạn ','2/9/2022','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150002','Còn Hạn ','8/4/2022','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150003','Còn Hạn ','5/3/2023','3');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150004','Còn Hạn ','12/12/2023','0');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150005','Còn Hạn ','11/13/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150006','Còn Hạn ','2/9/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150007','Còn Hạn ','8/4/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150008','Còn Hạn ','5/3/2023','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150009','Còn Hạn ','12/12/2023','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150010','Còn Hạn ','11/13/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150011','Còn Hạn ','2/9/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150012','Còn Hạn ','8/4/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150013','Còn Hạn ','5/3/2023','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150014','Còn Hạn ','12/12/2023','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150015','Còn Hạn ','11/13/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150016','Còn Hạn ','2/9/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150017','Còn Hạn ','12/12/2023','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150018','Còn Hạn ','11/13/2022','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150019','Còn Hạn ','2/9/2022','3');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150020','Còn Hạn ','12/12/2023','4');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150021','Còn Hạn ','11/13/2022','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150022','Còn Hạn ','2/9/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150023','Còn Hạn ','3/11/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150024','Còn Hạn ','1/31/2021','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150025','Còn Hạn ','4/23/2022','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150026','Còn Hạn ','2/8/2022','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150027','Còn Hạn ','6/4/2023','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150028','Còn Hạn ','1/24/2023','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150029','Còn Hạn ','4/25/2023','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150030','Còn Hạn ','6/22/2023','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150031','Còn Hạn ','2/16/2021','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150032','Còn Hạn ','10/27/2022','3');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150033','Còn Hạn ','12/7/2021','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150034','Còn Hạn ','2/2/2021','3');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150035','Còn Hạn ','10/26/2023','2');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150036','Còn Hạn ','8/15/2022','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150037','Còn Hạn ','5/8/2022','3');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150038','Còn Hạn ','3/12/2021','3');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150039','Còn Hạn ','9/6/2021','1');
+INSERT INTO THETHUVIEN (MaThe,TrangThai,NgayHetHan,SoLanGiaHan) VALUES ('17150040','Còn Hạn ','12/21/2023','1');
+
+-- Insert VIPHAM
+
+-- Insert DOCGIA
+
+-- Insert DAUSACH
+
+-- Insert SACH_TACGIA
+
+-- Insert PHIEUMUON
+
+-- Insert PHIEUTRA
+
+-- Insert CUONSACH
+
+-- Insert PHIEUKIEMKE
+
+-- Insert NHANVIEN
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2201','Nguyễn Thi Như Quỳnh ','221');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2202','Quách Văn Thắng','222');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2203','Lê Đình Tú','331');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2204','Nguyễn Ánh Dương','332');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2205','Trần Cao Thanh Tùng','433');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2206','Trương Anh Khoa','221');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2207','Đặng Quang Vũ','222');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2208','Nguyễn Bích Như','331');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2209','Vũ Kim Phượng','332');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2210','Nguyễn Phương Quyên','433');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2211','Phùng Ðức Bảo','221');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2212','Phạm Diệu Loan','332');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2213','Trần Ngọc Yến','222');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2214','Dương Thu Trang','221');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2215','Đoàn Bích Hằng','221');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2216','Võ Việt An','222');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2217','Nguyễn Khắc Duy','331');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2218','Phạm Phương Thảo','332');
+INSERT INTO NHANVIEN (MaNhanVien,TenNhanVien,MaKho) VALUES ('2219','Hồ Ngọc Huệ','433');
+
+
+-- Insert QUANLYMUONSACH
+
+-- Insert QUANLYTRASACH
+
+-- Insert PHIEUNHAP
+
+-- Insert PHIEUXUAT
